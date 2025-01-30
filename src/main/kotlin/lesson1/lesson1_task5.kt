e26ab2d58e27fe8122824743b1d5341da262ef9d
stan <stan@stanbox.space>
2025-01-31 02:43:04 +0300

    добавил константы

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,17 +1,21 @@
+/**
+ * Объяви переменную с количеством секунд, которые Гагарин провел в космосе.
+ *
+ * - Переведи в коде количество секунд в целые минуты и целые часы, сохраняя остаток секунд и минут в новые переменные;
+ * - Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды], например так: 01:30:09;
+ * - Значения часов, минут и секунд отображать двумя цифрами.
+ */
 package lesson1
+const val SECS_IN_MINUTES = 60
+const val MINUTES_IN_HOUR = 60
+const val SECS_IN_HOUR = MINUTES_IN_HOUR * SECS_IN_MINUTES
 
 fun main() {
-    /**
-     * Объяви переменную с количеством секунд, которые Гагарин провел в космосе.
-     *
-     * - Переведи в коде количество секунд в целые минуты и целые часы, сохраняя остаток секунд и минут в новые переменные;
-     * - Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды], например так: 01:30:09;
-     * - Значения часов, минут и секунд отображать двумя цифрами.
-     */
+
     val seconds = 6480
-    val hours = seconds / 3600
-    val minutes = (seconds % 3600) / 60
-    val lastSeconds = (seconds % 60) % 60
+    val hours = seconds / SECS_IN_HOUR
+    val minutes = (seconds % SECS_IN_HOUR) / MINUTES_IN_HOUR
+    val lastSeconds = (seconds % SECS_IN_HOUR) % MINUTES_IN_HOUR
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, lastSeconds)
     println(formattedTime)
 
